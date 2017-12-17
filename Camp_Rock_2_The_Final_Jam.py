aa30c5b31466b7531359f148df41ab76c8b60d83
smilkwilk <34556595+smilkwilk@users.noreply.github.com>
2017-12-16 22:08:49 -0800

    FIXED
    FIXED

--- Camp_Rock_2_The_Final_Jam.py
+++ Camp_Rock_2_The_Final_Jam.py
@@ -3,7 +3,10 @@ strategy_name = 'Camp Rock 2: The Final Jam'
 strategy_description = '''Junie Cortez's warty fingers'''
 
 def move(my_history, their_history, my_score, their_score):
-    if 'c' in their_history[-1]:
+    if len(my_history)==0:
+        return 'c'
+    prior_round = their_history[-1]
+    if prior_round == 'c':
         return 'c'
     else:
         return 'b'
